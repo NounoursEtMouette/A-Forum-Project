6fbbbc62a4b7b2b11d9c71605764a1c0da67b19a
xyreden <lefevre.flavien@live.com>
2018-08-17 21:15:25 +0200

    no message

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+            <link href="./css/style.css" rel="stylesheet">
+            <meta charset="utf-8">
+            <link href="http://fonts.googleapis.com/css?family=crete+round" rel="stylesheet">
+            <title>A VOIR</title>
+    </head>
+
+    <body>
+
+        <header>
+            <div class="wrapper">
+                <h1>La citadelle de sunor</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Actualités</a></li>
+                        <li><a href="#">Equipe</a></li>
+                        <li><a href="#">L'univers</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+
+            </div>
+        </header>
+
+        <section id="main-image">
+        </section>
+
+        <section id="bordureimage">
+
+        </section>
+
+        <section id="text-prez">
+            <p>In his tractibus navigerum nusquam visitur flumen sed in locis plurimis aquae suapte natura calentes emergunt ad usus aptae multiplicium medelarum. verum has quoque regiones pari sorte Pompeius Iudaeis domitis et Hierosolymis captis in provinciae speciem delata iuris dictione formavit.<br><br>
+
+                    Nemo quaeso miretur, si post exsudatos labores itinerum longos congestosque adfatim commeatus fiducia vestri ductante barbaricos pagos adventans velut mutato repente consilio ad placidiora deverti.<br><br>
+                    
+                    Nunc vero inanes flatus quorundam vile esse quicquid extra urbis pomerium nascitur aestimant praeter orbos et caelibes, nec credi potest qua obsequiorum diversitate coluntur homines sine liberis Romae.</p>
+        </section>
+        
+        <footer>
+            
+        </footer>
+
+    </body>
+
+</html>
