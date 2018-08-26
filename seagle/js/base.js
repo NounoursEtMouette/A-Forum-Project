ab85527c7491ece76de0fa2d2ea0426e659ad69e
xyreden <lefevre.flavien@live.com>
2018-08-26 13:38:29 +0200

    Création base appli tchat

---
+++ seagle/js/base.js
@@ -0,0 +1,15 @@
+function letsstart() {
+    $('#seagle-tchat').css('height', '30px');
+    $('#seagle-tchat').css('width', '100%');
+    $('#seagle-tchat').hover(
+        function() {
+            $(this).css("animation", "none");
+            $(this).css("height", "30px");   
+            $(this).css("cursor", "default");
+        }
+    );
+}
+
+function initialiseSeagleTchat() {
+    
+}
